0c3d2d6fcd8839eebd9ca5bb1937f746df10de9f
nikosid <nikosid@gmail.com>
2021-10-21 12:23:42 +0200

    Use some mui components

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import AddIcon from "@mui/icons-material/Add"
 import IconButton from "@mui/material/IconButton";
-import {Grid} from "@mui/material";
+import {Box, Container, Grid} from "@mui/material";
 
 const DUMMY_NOTE = {
   title: "",
@@ -27,18 +27,22 @@ const AddForm = ({onSubmit}) => {
     setIsOpen(true);
   };
   return (
-    <div>
-      <form>
-        {isOpen && <input type="text" name="title" placeholder="Title..." onChange={changeHandler} value={note.title}/>}
-        <p><textarea name="text" placeholder="Your note..." value={note.text} onChange={changeHandler}
-                     onClick={expandHandler} ></textarea></p>
-        <Grid container justifyContent="flex-end">
-          <IconButton onClick={submitHandler} aria-label="add">
-            <AddIcon/>
-          </IconButton>
-        </Grid>
-      </form>
-    </div>
+    <Container maxWidth="sm" className="adding">
+      <Box sx={{ bgcolor: '#fff', width: '600px' }}>
+        <form>
+          {isOpen && <input type="text" name="title" placeholder="Title..." onChange={changeHandler} value={note.title}/>}
+          <p><textarea name="text" placeholder="Your note..." value={note.text} onChange={changeHandler}
+                       onClick={expandHandler}/></p>
+          <Grid container justifyContent="flex-end">
+            <IconButton onClick={submitHandler} aria-label="add">
+              <AddIcon/>
+            </IconButton>
+          </Grid>
+        </form>
+      </Box>
+
+
+    </Container>
   );
 };
 
